7e94a3477dffc3d54acfeb6e0e8c876d72da1b16
10649926 <10649926@INFHYSZBL197.lntinfotech.com>
2021-01-13 10:56:00 +0530

    Updated

---
+++ AutomationLab/src/test/java/mav/AutomationLab/TC_01.java
@@ -0,0 +1,19 @@
+package mav.AutomationLab;
+
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_01 {
+	   @Test
+		public void testApp() {
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\10649926\\Downloads\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.w3schools.com/html/html_tables.asp");
+			driver.manage().window().maximize();
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("window.scrollBy(0,400)");
+	   }
+}
